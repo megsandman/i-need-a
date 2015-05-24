a185a01406aabd5ebafc90ed2423b58ab6b29811
Meg Sandman <margaret.sandman@gmail.com>
2015-05-23 21:46:40 -0700

    update url for get

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -16,7 +16,8 @@ $(document).ready(function() {
   function sendSelections(selectionObject) {
     $.ajax({
       type: 'GET',
-      url: 'http://localhost:3000/matches/',
+      url: 'https://ineeda.herokuapp.com/matches/',
+      // url: 'http://localhost:3000/matches/',
       data: selectionObject,
       success: function(response) {
         console.log("success")
