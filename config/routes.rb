acf3b70c8e59408335332a732a5a137cacdf47c4
Meg Sandman <margaret.sandman@gmail.com>
2015-06-02 10:36:48 -0700

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :matches, :only => [:index]
+  resources :genres, :only => [:index]
+  resources :activities, :only => [:index]
+  resources :genres, :only => [:index]
+
+
   root to: 'static#index'
 
 end
