038706066e9a4466c0705db2893c9dbc881ed6a9
Meg Sandman <margaret.sandman@gmail.com>
2015-06-18 21:35:16 -0700

    adjust order of genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,7 +3,7 @@ class GenresController < ApplicationController
 
   def show
     p params[:id]
-    neighborhoods = Genre.where(activity_id: params[:id])
+    neighborhoods = Genre.where(activity_id: params[:id]).order(id: :asc)
     render json: neighborhoods, status: 200
   end
 end
