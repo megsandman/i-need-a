e08df571fee47dee8d6cbb10f35dee002090f6ab
Meg Sandman <margaret.sandman@gmail.com>
2015-06-23 16:41:08 -0700

    edit cities#index

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,8 +1,9 @@
 class CitiesController < ApplicationController
+  require 'json'
 
   def index
     @cities = City.all
-    render json => @cities, status: 200
+    render :json => @cities, status: 200
   end
 
 end
