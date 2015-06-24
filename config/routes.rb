34bae3c01fdc8c780568ef8b4e46e10c64fef9f1
Meg Sandman <margaret.sandman@gmail.com>
2015-06-24 16:57:37 -0700

    adjust neighborhoods index route to be nested under cities

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :genres, :only => [:show]
   resources :neighborhoods, :only => [:index]
   resources :imports, :only => [:index]
-  resources :cities, :only => [:index, :show]
+  resources :cities, :only => [:index] do
+    resources :neighborhoods, :only => [:index]
+  end
   resources :websites, :only => [:index]
   resources :matches, :only => [:index, :show, :update, :new, :create]
 
