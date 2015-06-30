ae0d27dcd9c5f2e70fef5faa87fd73ebfbb3950d
Meg Sandman <margaret.sandman@gmail.com>
2015-06-29 22:52:38 -0700

    fix change to neighborhoods#index that messed up old version of app

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -2,7 +2,11 @@ class NeighborhoodsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    @neighborhoods = Neighborhood.where(city_id: params[:city_id])
+    if params[:city_id]
+      @neighborhoods = Neighborhood.where(city_id: params[:city_id])
+    else
+      @neighborhoods = Neighborhood.where(city_id: 1)
+    end
     render :json => @neighborhoods, status: 200
   end
 
